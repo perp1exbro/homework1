125607b758c547ead6942309e052b86ae9ac83eb
perp1exbro <137335593+perp1exbro@users.noreply.github.com>
2023-07-12 06:20:52 +0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Таблица кубов:\n");
+for (int i=1;i<=n;i++)
+{
+int k = i*i*i;
+Console.Write($"{k}, ");
+}
